4536ce62f4549ba1714f6fe36d3af54c0c53afec
xuhui <zmm064@foxmail.com>
2018-09-02 10:13:05 +0800

    添加项目文件。

---
+++ Django论坛/__init__.py
@@ -0,0 +1,3 @@
+﻿"""
+Package for Django论坛.
+"""
---
+++ Django论坛/urls.py
@@ -0,0 +1,21 @@
+﻿"""
+Definition of urls for Django论坛.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Django论坛.views.home, name='home'),
+    # url(r'^Django论坛/', include('Django论坛.Django论坛.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
