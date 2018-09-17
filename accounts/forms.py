3b61c05d03bf9b9556517d02860e7aee35ca24e0
xuhui <zmm064@foxmail.com>
2018-09-17 09:25:50 +0800

    部署2

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+#from .models import Profile
 
 class SignUpForm(UserCreationForm):
     # 显式声明，要求邮箱必须填
@@ -11,9 +11,9 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('url', 'location', 'company')
+#class ProfileForm(forms.ModelForm):
+#    class Meta:
+#        model = Profile
+#        fields = ('url', 'location', 'company')
 
 
