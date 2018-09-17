8310824bab0bdd4653fa7187b6ff8e37ddd7fb3a
xuhui <zmm064@foxmail.com>
2018-09-17 09:49:53 +0800

    部署3

--- Django论坛/settings.py
+++ Django论坛/settings.py
@@ -130,7 +130,7 @@ LOGIN_REDIRECT_URL = 'boards:home'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
 ]
